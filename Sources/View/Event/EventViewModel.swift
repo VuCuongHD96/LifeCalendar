6dcf9c21c6bd0f2b08795b056298312e5747c95f
VuCuongHD96 <xuancuonghd1102@gmail.com>
2024-07-05 04:36:25 +0700

    event view slide

---
+++ Sources/View/Event/EventViewModel.swift
@@ -0,0 +1,83 @@
+//
+//  EventViewModel.swift
+//
+//
+//  Created by Work on 5/7/24.
+//
+
+import Combine
+
+struct EventViewModel {
+    
+    struct Constant {
+        static let hourUnit = 80
+    }
+    
+    let event: EventViewData
+}
+
+struct EventSlideOffset {
+    
+    var offset: Float
+    var lastOffset: Float
+    
+    init(offset: Float, lastOffset: Float) {
+        self.offset = offset
+        self.lastOffset = lastOffset
+    }
+    
+    init() {
+        self.init(offset: .zero, lastOffset: .zero)
+    }
+}
+
+extension EventViewModel: ViewModel {
+    
+    class Input: ObservableObject {
+        let loadTrigger = Driver.just(Void())
+        @Published var slideViewPaddingTop = 0
+        @Published var eventViewPaddingTop = 0
+        
+        @Published var eventOffset: EventSlideOffset = .init()
+    }
+    
+    class Output: ObservableObject {
+        @Published var event: EventViewData = .init()
+        @Published var dueration = 0
+        @Published var eventPreviewHeight = 0
+        @Published var eventPreviewPaddingTop = 0
+        
+        @Published var eventViewPadding = 0
+    }
+    
+    func transform(_ input: Input, cancelBag: CancelBag) -> Output {
+        let output = Output()
+        
+        Just(event)
+            .assign(to: \.event, on: output)
+            .store(in: cancelBag)
+        
+        input.loadTrigger
+            .map {
+                event.end - event.start - 1
+            }
+            .assign(to: \.dueration, on: output)
+            .store(in: cancelBag)
+        
+        output.$dueration
+            .map {
+                ($0 + 1) * Constant.hourUnit + $0
+            }
+            .assign(to: \.eventPreviewHeight, on: output)
+            .store(in: cancelBag)
+        
+        output.$event
+            .map {
+                $0.start * Constant.hourUnit + $0.start
+            }
+            .assign(to: \.eventPreviewPaddingTop, on: output)
+            .store(in: cancelBag)
+        
+        return output
+    }
+}
