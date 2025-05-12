2111227e032fda4961a745031730fb91a8a45982
Cuongvx <vucuonghd1996@gmail.com>
2025-05-12 22:26:54 +0700

    date inline popover date picker

--- Sources/Calendar/LifeDatePicker.swift
+++ Sources/Calendar/LifeDatePicker.swift
@@ -11,20 +11,25 @@ struct LifeDatePicker: View {
     
     @Binding var dateSelected: Date
     @Environment(\.locale) var locale
+    @State private var isDatePickerShown = false
     
     var body: some View {
-        ZStack {
-            DatePicker("", selection: $dateSelected, displayedComponents: .date)
-                .labelsHidden()
-                .opacity(0.1)
-                .environment(\.locale, locale)
-            Text(CalendarManager.createDateString(from: dateSelected, locale: locale))
-                .fontWeight(.medium)
-                .padding(8)
-                .foregroundColor(.white)
-                .background(RoundedRectangle(cornerRadius: 15, style: .continuous).foregroundColor(.blue))
-                .allowsHitTesting(false)
-        }
+        Text(CalendarManager.createDateString(from: dateSelected, locale: locale))
+            .fontWeight(.medium)
+            .padding(8)
+            .foregroundColor(.white)
+            .background(RoundedRectangle(cornerRadius: 15, style: .continuous).foregroundColor(.blue))
+            .onTapGesture {
+                isDatePickerShown.toggle()
+            }
+            .popover(isPresented: $isDatePickerShown) {
+                DatePicker("", selection: $dateSelected, displayedComponents: .date)
+                    .labelsHidden()
+                    .environment(\.locale, locale)
+                    .datePickerStyle(.graphical)
+                    .presentationCompactAdaptation(.popover)
+                    .frame(width: 300, height: 310)
+            }
     }
 }
 
