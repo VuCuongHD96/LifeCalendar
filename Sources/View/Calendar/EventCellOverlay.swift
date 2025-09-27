418704ba68f998ad251f19b7f10997d6fb704eeb
Cuongvx <vucuonghd1996@gmail.com>
2025-09-27 15:42:37 +0700

    fix bug event before or after this day

--- Sources/View/Calendar/EventCellOverlay.swift
+++ Sources/View/Calendar/EventCellOverlay.swift
@@ -11,11 +11,12 @@ struct EventCellOverlay: View {
     
     typealias HeightHandler = (CGFloat) -> Void
     
+    let dateSelected: Date
     var event: EventCellData
     @Binding var eventOffset: EventOffset
     
     var body: some View {
-        EventCell(event: event, opacity: 1)
+        EventCell(dateSelected: dateSelected, event: event, opacity: 1)
             .padding(.top, eventOffset.totalOffset)
             .gesture(
                 EventCellDragGesture(eventOffset: $eventOffset)
