f3edffa5642a93ad6c7710f48cba4959188ef1b2
Cuongvx <vucuonghd1996@gmail.com>
2026-01-14 14:47:26 +0700

    calendar inline call back date

--- Sources/Calendar/CalendarDateRangeView/CalendarDateRangeView.swift
+++ Sources/Calendar/CalendarDateRangeView/CalendarDateRangeView.swift
@@ -10,16 +10,19 @@ import SwiftDate
 
 public struct CalendarDateRangeView: View {
     
+    // MARK: - Define
+   public typealias DateHander = (DateInterval) -> Void
+    
     // MARK: - Property
     @Environment(\.locale) var locale
-    @Binding var startDate: Date
-    @Binding var endDate: Date
-    
-    public init(startDate: Binding<Date>, endDate: Binding<Date>) {
-        _startDate = startDate
-        _endDate = endDate
-    }
+    @State private var startDate: Date = Date.now
+    @State private var endDate: Date = Date.now + 1.days
+    let dateIntervalHandler: DateHander
 
+    public init(dateIntervalHandler: @escaping DateHander) {
+        self.dateIntervalHandler = dateIntervalHandler
+    }
+    
     public var body: some View {
         Section {
             DatePicker("Start date",
@@ -33,16 +36,30 @@ public struct CalendarDateRangeView: View {
         .environment(\.locale, locale)
         .listRowInsets(EdgeInsets())
         .padding(.horizontal, 8)
+        .onChange(of: startDate) { oldValue, newValue in
+            handlerDateInterval(startDate: newValue, endDate: endDate)
+        }
+        .onChange(of: endDate) { oldValue, newValue in
+            handlerDateInterval(startDate: startDate, endDate: newValue)
+        }
+        .onAppear {
+            handlerDateInterval(startDate: startDate, endDate: endDate)
+        }
+    }
+    
+    private func handlerDateInterval(startDate: Date, endDate: Date) {
+        if startDate < endDate {
+            let dateInterval = DateInterval(start: startDate, end: endDate)
+            dateIntervalHandler(dateInterval)
+        } else {
+            self.endDate = startDate + 1.days
+        }
     }
 }
 
 #Preview {
     
-    @Previewable
-    @State var startDate = Date.now
-    
-    @Previewable
-    @State var endDate = Date.now.tomorrow
-    
-    CalendarDateRangeView(startDate: $startDate, endDate: $endDate)
+    CalendarDateRangeView(dateIntervalHandler: {
+        print("--- date interval = ", $0)
+    })
 }
